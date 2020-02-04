2a273b7474689d137e3337c3dbe4e84715d644fa
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-04 02:15:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,7 @@ class Anagram
     @word = word
   end
   
-  def match(array_of_words)
 
-    array_of_words.select do |element|
-      (@word.split("").sort) == (element.split("").sort)
-    end
-  end
  
   
   
